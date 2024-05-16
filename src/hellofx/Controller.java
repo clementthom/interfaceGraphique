904ce7e7595f8bec00028121873a7afed12bccca
clementthom <50517019+clementthom@users.noreply.github.com>
2024-05-16 13:30:54 +0200

    Problème de liaison contrôleur-fxml résolu Début d'écriture des fonctions handle

--- src/hellofx/Controller.java
+++ src/hellofx/Controller.java
@@ -2,11 +2,25 @@ package hellofx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
 
 public class Controller {
 
     @FXML
     private Label label;
+
+    //plan menu principal
+    protected void handleModeAutomatiqueButtonAction(ActionEvent e) {
+
+    } 
+    @FXML
+    protected void handleModeManuelButtonAction(ActionEvent e) {
+
+    } 
+    @FXML
+    protected void handleArretButtonAction(ActionEvent e) {
+
+    } 
 
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
